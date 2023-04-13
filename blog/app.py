ab6df0e1cc1179c64c174fc4145101192a22943a
lavrov-alexey <lavrov-box@mail.ru>
2023-04-13 22:53:28 +0300

    Lesson-01. Tasks finished

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,4 @@
-from datetime import time
+from time import time
 
 from flask import Flask, g
 from flask import request
@@ -23,14 +23,15 @@ app = Flask(__name__)
 # def index(some_id: int):
 #     return f'Идентификатор {some_id}!'
 
+
 # добавление query-параметров в пути (например http://127.0.0.1:5005/123 или http://127.0.0.1:5005/123?name=Alex)
 # также опционально - можно указать доступные методы для этого эндпоинта
-@app.route('/<int:some_id>', methods=['GET', 'POST'])
-def index(some_id: int):
+@app.route('/<string:some_str>', methods=['GET', 'POST'])
+def index(some_str: str):
     # из словаря query-параметров запроса (request.args) можем получить по имени нужный параметр станд. методом словаря
-    name = request.args.get('name', None)
-    return f'Идентификатор {some_id}, переданное в параметре имя (name): {name}, метод запроса: {request.method=}'
-    # Идентификатор 123, переданное в параметре имя (name): Alex
+    test_param = request.args.get('test_param', None)
+    return f'Строка в url {some_str}, переданный параметр (test_param): {test_param}, метод запроса: {request.method=}'
+
 
 # декараторы before_request и after_request - позволяют выполнять опред. логику до и/или после работы flask
 @app.before_request
@@ -38,9 +39,18 @@ def fix_time_start_app():
     # в рамках каждого запроса есть свой "глобальный" объект g, в кот. можно хранить необходимые данные
     g.start_time = time()
 
+
 @app.after_request
 def calc_time_app(response):
     # получаем из g время начала вып. запроса, вычисляем время выполнения запроса flask и возвр. его в заголовках
-    if hasattr(g, 'start_time'):
-        response.headers['process-time'] = time() - g.start_time
+    if hasattr(g, "start_time"):
+        response.headers["process-time"] = time() - g.start_time
     return response
+
+
+# создаем свой обработчик для кода ошибки 404
+@app.errorhandler(404)
+def handler_404(curr_error):
+    # регистрируем ошибку во встроенном логгере приложения (уровень - error)
+    app.logger.error(curr_error)
+    return '404'
