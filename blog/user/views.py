8dbf40844159e127498873b9bd5efd04b2e12cf6
lavrov-alexey <lavrov-box@mail.ru>
2023-04-15 09:08:17 +0300

    Lesson-02. Add bootstrap

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,22 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
+from werkzeug.exceptions import NotFound
 
 # создаем эскиз user, параметр url_prefix позволит обратиться к url "localhost/users"
 user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
 
 # Пока не работаем с БД - создадим глобальную константу с данными о пользователях
-USERS = ['Иван', 'Мария', 'Владимир']
+# USERS = ['Иван', 'Мария', 'Владимир']  # вариант простого списка
+# вариант имитации объекта с id (pk)
+USERS = {
+    1: 'Иван',
+    2: 'Мария',
+    3: 'Владимир',
+}
 
 
 # регистрируем роут в созданном ранее блюпринте юзер, т.е. обращение будет к корню
 # урла "localhost/users/"
 @user.route('/')
 def user_list():
-    # вызываем функцию отрисовки шаблона и передаем в контекст наших пользователей, чтобы исп-вать в шаблоне
+    # вызываем функцию отрисовки шаблона и передаем в контекст шаблона наших пользователей, чтобы исп-вать в шаблоне
     return render_template(
         'users/list.html',
         users=USERS,
     )
 
-@user.route('/<pk>')
+@user.route('/<int:pk>')
 def get_user(pk: int):
-    return pk
+    # обработка варианта передачи несуществующего id
+    try:
+        user_name = USERS[pk]
+    except KeyError:
+        # вариант с обработкой и отображением ошибки
+        # raise NotFound(f'User id {pk} not found!')
+        # вариант с перенаправлением польвателя например на список пользователей
+        return redirect('/users/')
+    return render_template(
+        'users/details.html',
+        user_name=user_name,
+    )
